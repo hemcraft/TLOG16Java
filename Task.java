b10ec5c1b6a8dcd80c802c4ec5c778271dbc2dca
hemcraft <hemcraft@gmail.com>
2017-06-27 18:44:41 +0200

    Add classes

---
+++ Task.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tlog16java;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author Andris
+ */
+public class Task {
+    String taskId;
+    LocalTime startTime;
+    LocalTime endTime;
+    String comment;
+    
+}
---
+++ WorkMonth.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tlog16java;
+
+import java.time.YearMonth;
+import java.util.List;
+
+/**
+ *
+ * @author Andris
+ */
+public class WorkMonth {
+    List<WorkDay> days;
+    YearMonth date;
+    long sumPerMonth;
+    
+}
